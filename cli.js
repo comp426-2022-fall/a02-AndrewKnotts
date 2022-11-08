53896fcb9d6dccb31f3fd4542f1ca383dcafa38f
Andrew Knotts <knottsan2010@gmail.com>
2022-11-08 18:05:09 -0500

    atttempt6

--- cli.js
+++ cli.js
@@ -5,19 +5,24 @@ import minimist from 'minimist';
 import moment from 'moment-timezone';
 import fetch from 'node-fetch';
 
-const args = minimist(proccess.argv.slice(2));
+const args = minimist(process.argv.slice(2));
 
 
 if(args.h){
-    console.log('Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE')
-    console.log('-h            Show this help message and exit.')
-    console.log('-n, -s        Latitude: N positive; S negative.')
-    console.log('-e, -w        Longitude: E positive; W negative.')
-    console.log('-z            Time zone: uses tz.guess() from moment-timezone by default.')
-    console.log('-d 0-6        Day to retrieve weather: 0 is today; defaults to 1.')
-    console.log('-j            Echo pretty JSON from open-meteo API and exit.')
-
-proccess.exit(0);
+  try{
+    console.log(`
+    Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE
+    -h            Show this help message and exit.
+    -n, -s        Latitude: N positive; S negative.
+    -e, -w        Longitude: E positive; W negative.
+    -z            Time zone: uses tz.guess() from moment-timezone by default.
+    -d 0-6        Day to retrieve weather: 0 is today; defaults to 1.
+    -j            Echo pretty JSON from open-meteo API and exit.
+  `)
+  proccess.exit(0);
+  } catch(err){
+     process.exitCode(1);
+  } 
 }
 
 let timezone = moment.tz.guess();
