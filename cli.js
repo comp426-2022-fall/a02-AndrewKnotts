572afeb42e3f27e6bb8de2fdee5a402244bc1b60
Andrew Knotts <knottsan2010@gmail.com>
2022-11-08 18:14:38 -0500

    attempt7

--- cli.js
+++ cli.js
@@ -9,20 +9,15 @@ const args = minimist(process.argv.slice(2));
 
 
 if(args.h){
-  try{
-    console.log(`
-    Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE
-    -h            Show this help message and exit.
-    -n, -s        Latitude: N positive; S negative.
-    -e, -w        Longitude: E positive; W negative.
-    -z            Time zone: uses tz.guess() from moment-timezone by default.
-    -d 0-6        Day to retrieve weather: 0 is today; defaults to 1.
-    -j            Echo pretty JSON from open-meteo API and exit.
-  `)
-  proccess.exit(0);
-  } catch(err){
-     process.exitCode(1);
-  } 
+    console.log('Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE')
+    console.log('-h            Show this help message and exit.')
+    console.log('-n, -s        Latitude: N positive; S negative.')
+    console.log('-e, -w        Longitude: E positive; W negative.')
+    console.log('-z            Time zone: uses tz.guess() from moment-timezone by default.')
+    console.log('-d 0-6        Day to retrieve weather: 0 is today; defaults to 1.')
+    console.log('-j            Echo pretty JSON from open-meteo API and exit.')
+
+process.exit(0);
 }
 
 let timezone = moment.tz.guess();
@@ -32,16 +27,15 @@ let longitude = args.e || args.w * -1;
 
 
 // Make a request
-const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=' + latitude + '&longitude=' + longitude + '&hourly=temperature_2m,relativehumidity_2m,precipitation,surface_pressure&current_weather=true&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timezone=America%2FNew_York&past_days=7');
+const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=' + latitude + '&longitude=' + longitude + '&hourly=temperature_2m&current_weather=true&timezone=' + timezone);
 // Get the data from the request
 const data = await response.json();
 
 if (args.j){
     console.log(data);
-    proccess.exit(0);
+    process.exit(0);
 }
 
-console.log(data);
 console.log(data.precipitation);
 
 const days = args.d 
